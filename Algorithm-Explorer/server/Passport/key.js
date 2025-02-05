7dbcdfdfba12eb822effaf6c64be2bdf64e0177b
ShivamPratap16 <138383845+ShivamPratap16@users.noreply.github.com>
2025-02-05 19:13:27 +0530

    first commit

--- Algorithm-Explorer/server/Passport/key.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    google: {
-      clientID: '469011518203-7oquirgrfgf4u1v385b7co0j39o3no5e.apps.googleusercontent.com',
-      clientSecret: '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',
-      callbackURL: 'http://localhost:4000/auth/google/callback'
-    }
-  };
